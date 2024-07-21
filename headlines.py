f7ffabdef4c3fa761918dacba1cfa7de412e9fed
EbiScott <egbejuleeworitse@gmail.com>
2024-07-21 23:49:12 +0100

    updated headlines.oy

--- headlines.py
+++ headlines.py
@@ -1,26 +1,27 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {
+             'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'
+            }
 
 @app.route("/")
 def get_news():
-    feed = feedparser.parse(BBC_FEED)
-    first_article = feed['entries'][0]
-    return """
-        <html>
-            <body>
-                <h1> BBC Headlines </h1>
-                <b>{0}</b> <br/>
-                <i>{1}</i> <br/>
-                <p>{2}</p> <br/>
-            </body>
-        </htmh>
-    """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    query = request.args.get(publication)
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+        
+    feed = feedparser.parse(RSS_FEEDS[publication])
 
+    return render_template('home.html', articles=feed['entries'])
 
 if __name__ == "__main__":
     app.run(debug=True)
