fa69d8b40a89e2a705f27cecfe55afec6dbce0f5
EbiScott <egbejuleeworitse@gmail.com>
2024-07-22 21:14:38 +0100

    Using GET

--- headlines.py
+++ headlines.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 
 RSS_FEEDS = {
              'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'cnn': 'http://rss.cnn.com/rss/edition_world.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'
+             'iol': 'http://www.iol.co.za/cmlink/1.640',
+             'nbc': 'https://www.nbcnews.com/id/3032091/device/rss/rss.xml',
+             'cnbc': 'https://www.cnbc.com/id/100727362/device/rss/rss.html',
+             'abc': 'http://abcnews.go.com/abcnews/internationalheadlines',
+             'aljazeera': 'http://www.aljazeera.com/xml/rss/all.xml',
+             'cbs': 'https://www.cbsnews.com/latest/rss/world'
             }
 
 @app.route("/")
