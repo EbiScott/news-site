8875d293d70fb062b92cb53c99fe51226fcc6a4f
EbiScott <egbejuleeworitse@gmail.com>
2024-07-24 17:27:29 +0100

    updated headlines.py

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
+import datetime
 from dotenv import load_dotenv
 import feedparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import json
 import random
 import urllib
@@ -25,39 +26,48 @@ RSS_FEEDS = {
 
 
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=OPENEXCHANGE_API"
 
-# def get_weather(query):
-#     API_KEY = 'OPENWEATHER_API'
-#     query = urllib.quote(query)
-#     url = api_url.format(query)
-#     data = urllib2.urlopen(url).read()
-#     parsed = json.loads(data)
-#     weather = None
-#     if parsed.get("weather"):
-#         weather = {"description":parsed["weather"][0]["description"], "temperature":parsed["main"]["temp"], "city":parsed["name"]}
-
-DEFAULTS = {'publication': 'bbc', 'city': 'Lagos,NG'}
+DEFAULTS = {'publication': 'bbc', 'city': 'London,UK', 'currency_from':'GBP', 'currency_to':'USD'}
 
 
 @app.route("/")
 def home():
-    publication = request.args.get('publication')
+    publication = request.args.get("publication")
     if not publication:
-        publication = DEFAULTS['publication']
+        publication = request.cookies.get("publication")
+        if not publication:
+            publication = DEFAULTS["publication"]
     articles = get_news(publication)
 
     city = request.args.get('city')
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather)
+
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+
+    
+
+    response = make_response(render_template("home.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
+    
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+
 
 def get_news(publication):
-    # query = request.args.get(publication)
-    # if not query or query.lower() not in RSS_FEEDS:
-    #     publication = DEFAULTS["publication"]
-    # else:
-    #     publication = query.lower()
+    
     feed = feedparser.parse(RSS_FEEDS[publication])
     return feed['entries']
 
@@ -71,9 +81,19 @@ def get_weather(query):
     if response.status == 200:
         data = json.loads(response.data)
         if data.get('weather'):
-            weather = {'description': data['weather'][0]['description'], 'temperature': data['main']['temp'], 'city': data['name']}
+            weather = {'description': data['weather'][0]['description'], 'temperature': data['main']['temp'], 'city': data['name'], 'country': data['sys']['country']}
             return weather
     return None 
+
+def get_rate(frm, to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read() #need to fix this urllib2 to urllib3 issue here
+
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
