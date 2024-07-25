9a17abbf0d8939fbb3269c21715fb231164f1388
EbiScott <egbejuleeworitse@gmail.com>
2024-07-25 23:04:10 +0100

    simplified my home function

--- headlines.py
+++ headlines.py
@@ -30,39 +30,36 @@ CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=OPENEXCHAN
 
 DEFAULTS = {'publication': 'bbc', 'city': 'London,UK', 'currency_from':'GBP', 'currency_to':'USD'}
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+
+    return DEFAULTS[key]
 
 @app.route("/")
 def home():
-    publication = request.args.get("publication")
-    if not publication:
-        publication = request.cookies.get("publication")
-        if not publication:
-            publication = DEFAULTS["publication"]
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
 
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
-    weather = get_weather(city)
+    city = get_value_with_fallback("city")
+    weather = get_weather (city)
 
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate, currencies = get_rate(currency_from, currency_to)
 
-    
-
     response = make_response(render_template("home.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
-    
+
     expires = datetime.datetime.now() + datetime.timedelta(days=365)
 
     response.set_cookie("publication", publication, expires=expires)
     response.set_cookie("city", city, expires=expires)
     response.set_cookie("currency_from", currency_from, expires=expires)
     response.set_cookie("currency_to", currency_to, expires=expires)
+
     return response
 
 
